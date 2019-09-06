d3b73d47c01d73639b0a37f615258a16e9f4f4f8
Chris Harrison <c.harrison1988@gmail.com>
2019-09-06 21:37:29 +0100

    Add initial test skeleton

---
+++ src/DateValidatorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ChrisHarrison\DateValidator;
+
+use PHPUnit\Framework\TestCase;
+
+final class DateValidatorTest extends TestCase
+{
+    public function test_it_allows_valid_leap_year_days()
+    {
+        
+    }
+
+    public function test_it_disallows_invalid_leap_year_days()
+    {
+
+    }
+
+    public function test_it_disallows_impossible_dates()
+    {
+
+    }
+
+    public function test_it_allows_past_dates()
+    {
+
+    }
+
+    public function test_it_disallows_current_date()
+    {
+
+    }
+
+    public function test_it_disallows_future_dates()
+    {
+
+    }
+
+    public function test_it_disallows_invalid_format_dates()
+    {
+
+    }
+}
